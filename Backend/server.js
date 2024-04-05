7c3645592beb68203f008666763bd3cc32d37849
Aryan Singh <aryansingh6011@gmail.com>
2024-04-06 02:17:33 +0530

    InitalCommit

--- Backend/server.js
+++ Backend/server.js
@@ -1,47 +1,32 @@
 const express = require("express");
+const { MongoClient } = require("mongodb");
+
 const app = express();
 const port = 3000;
-const { PrismaClient } = require("@prisma/client");
-const prisma = new PrismaClient();
 
+// MongoDB connection URL
+const uri =
+  "mongodb+srv://Admin:syntaxerreur@cluster0.bggk7ao.mongodb.net/Cluster0";
+const client = new MongoClient(uri);
+
+// Connect to MongoDB
+async function connectToMongoDB() {
+  try {
+    await client.connect();
+    console.log("Connected to MongoDB");
+  } catch (err) {
+    console.error("Error connecting to MongoDB:", err);
+  }
+}
+
+connectToMongoDB();
+
+// Express routes
 app.get("/", (req, res) => {
   res.send("Hello World!");
 });
 
+// Start the server
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
 });
-
-async function main() {
-  console.log("Starting main function");
-  await prisma.user.create({
-    data: {
-      name: "Rich",
-      email: "hello@prisma.com",
-      posts: {
-        create: {
-          title: "My first post",
-          body: "Lots of really interesting stuff",
-          slug: "my-first-post",
-        },
-      },
-    },
-  });
-
-  const allUsers = await prisma.user.findMany({
-    include: {
-      posts: true,
-    },
-  });
-  console.dir(allUsers, { depth: null });
-}
-
-main()
-  .then(async () => {
-    await prisma.$disconnect();
-  })
-  .catch(async (e) => {
-    console.error(e);
-    await prisma.$disconnect();
-    process.exit(1);
-  });
