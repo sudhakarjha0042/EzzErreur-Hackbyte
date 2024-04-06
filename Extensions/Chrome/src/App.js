32a7508cee65f4358c107f4dbca67daf162d6b0a
Aryan Singh <aryansingh6011@gmail.com>
2024-04-07 01:26:48 +0530

    Extension Changes

--- Extensions/Chrome/src/App.js
+++ Extensions/Chrome/src/App.js
@@ -1,6 +1,23 @@
+import React, { useEffect, useState } from "react";
 import logo from "./logo.svg";
 
 function App() {
+  const [currentTabUrl, setCurrentTabUrl] = useState("");
+
+  useEffect(() => {
+    // Check if the window.chrome object is available (running in a Chrome extension context)
+    if (window.chrome && window.chrome.tabs) {
+      window.chrome.tabs.query(
+        { active: true, lastFocusedWindow: true },
+        (tabs) => {
+          if (tabs.length > 0) {
+            setCurrentTabUrl(tabs[0].url);
+          }
+        }
+      );
+    }
+  }, []);
+
   return (
     <div
       className="App"
@@ -8,10 +25,14 @@ function App() {
         display: "flex",
         height: "100%",
         justifyContent: "center",
+        flexDirection: "column",
+        alignItems: "center",
       }}
     >
-      Hey this is Me and my extension Hey this is Me and my extension Hey this
-      is Me and my extension
+      <p>Hey this is Me and my extension</p>
+      <p>Hey this is Me and my extension</p>
+      <p>Hey this is Me and my extension</p>
+      <p>Current Tab URL: {currentTabUrl}</p>
     </div>
   );
 }
