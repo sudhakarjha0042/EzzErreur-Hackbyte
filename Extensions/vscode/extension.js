fb65f9dd78457dd50a8e6de2b56086a4f9a70d8e
VedantS73 <vedantsawant72003@gmail.com>
2024-04-06 23:04:01 +0530

    vscode extension upgrade

--- Extensions/vscode/extension.js
+++ Extensions/vscode/extension.js
@@ -3,6 +3,10 @@ const fs = require('fs');
 const path = require('path');
 const FlatListProvider = require('./flatListProvider');
 const axios = require('axios');
+const OpenAI = require('openai')
+
+const openai = new OpenAI({ apiKey: 'khudka use krow' });
+
 
 /**
  * @param {vscode.ExtensionContext} context
@@ -35,6 +39,8 @@ async function activate(context) {
 
 		console.log("-----------------------------------")
 		console.log(node.label)
+		console.log(node.description)
+		console.log(node.extension)
 		console.log(node.tags)
 		console.log(node.code)
 		console.log("-----------------------------------")
@@ -44,48 +50,96 @@ async function activate(context) {
 		<!DOCTYPE html>
 		<html lang="en">
 		<head>
-		<meta charset="UTF-8">
-		<meta name="viewport" content="width=device-width, initial-scale=1.0">
-		<title>${node.label}</title>
+			<meta charset="UTF-8">
+			<meta name="viewport" content="width=device-width, initial-scale=1.0">
+			<title>${node.label}</title>
+			<!-- Bootstrap CSS -->
+			<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet">
+			<style>
+				/* Additional custom styles can be added here */
+				.code-snippet {
+					background-color: #343a40; /* Dark background color */
+					color: #ffffff; /* Light text color */
+					padding: 1rem; /* Add padding for better readability */
+					border-radius: 0.25rem; /* Add border radius for rounded corners */
+					overflow-x: auto; /* Allow horizontal scrolling for long code */
+				}
+				.badge-blue {
+					background-color: #007bff;
+				}
+				.badge-grey {
+					background-color: #6c757d;
+				}
+    		</style>
 		</head>
-		<body>
-		<h1>Code Title : ${node.label}</h1>
-		<h2>Code Tags : ${node.tags.join(', ')}</h2>
-		<h2>Code Snippet:</h2>
-		<blockquote><pre><code>${node.code.replace(/"/g, '&quot;')}</code></pre></blockquote>
-		<button onclick="copyToClipboard('${node.code}')">Copy to Clipboard</button>
-		<button onclick="deleteSnippet('${node.label}')">Delete Snippet</button>
-		<button onclick="uploadSnippet('${node.label}', '${node.tags}', '${node.code}')">Upload Snippet</button>
+		<body class="container py-4">
+			<div class="mb-4">
+				<h1 class="h4">EZZ ERREUR - <a href="www.google.com" class="link-primary">View in Browser</a></h1>
+			</div>
+			<hr>
+			<div class="mb-4">
+				<h2 class="h5">Code Title:</h2>
+				<p class="lead">${node.label}</p>
+			</div>
+			<div class="mb-4">
+				<h2 class="h5">Code Description:</h2>
+				<p>${node.description}</p>
+			</div>
+			<div class="mb-4">
+				<h2 class="h5">Code Tags:</h2>
+				<div>
+					${node.tags.map((tag, index) => `
+					<span class="badge ${index % 2 === 0 ? 'badge-blue' : 'badge-grey'} me-2">${tag}</span>
+					`).join('')}
+				</div>
+			</div>
+			<div class="mb-4">
+				<h2 class="h5">Code Extension:</h2>
+				<p>${node.extension}</p>
+			</div>
+			<hr>
+			<div class="mb-4">
+				<h2 class="h5">Code Snippet:</h2>
+				<pre class="code-snippet"><code>${node.code.replace(/"/g, '&quot;')}</code></pre>
+			</div>
+			<hr>
+			<div class="d-grid gap-2 d-md-flex justify-content-md-start mb-4">
+				<button class="btn btn-primary me-md-2" onclick="copyToClipboard('${node.code}')">Copy to Clipboard</button>
+				<button class="btn btn-danger me-md-2" onclick="deleteSnippet('${node.label}')">Delete Snippet</button>
+				<button class="btn btn-success" onclick="uploadSnippet()">Upload Snippet</button>
+			</div>
+			<!-- Bootstrap JS (Optional, if needed) -->
+			<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.bundle.min.js"></script>
+			<script>
+				function copyToClipboard(text) {
+					navigator.clipboard.writeText(text);
+				}
+				function deleteSnippet(label) {
+					acquireVsCodeApi.postMessage({
+						command: 'deleteSnippet',
+						label: label
+					});
+				}
+			
+				function uploadSnippet() {
+					acquireVsCodeApi.postMessage({
+						command: 'uploadSnippet',
+						label: label,
+						tags: tags,
+						code: code
+					});
+				}
+		
+				function acquireVsCodeApi() {
+					if (typeof acquireVsCodeApi !== 'undefined') {
+					return acquireVsCodeApi();
+					} else {
+					const vscode = acquireVsCodeApi();
+					return vscode;
+					}
+				}
+			</script>
 		</body>
-		<script>
-		function copyToClipboard(text) {
-			navigator.clipboard.writeText(text);
-		}
-		function deleteSnippet(label) {
-			acquireVsCodeApi.postMessage({
-				command: 'deleteSnippet',
-				label: label
-			});
-		}
-	
-		function uploadSnippet(label, tags, code) {
-			acquireVsCodeApi.postMessage({
-				command: 'uploadSnippet',
-				label: label,
-				tags: tags,
-				code: code
-			});
-		}
-
-		function acquireVsCodeApi() {
-			if (typeof acquireVsCodeApi !== 'undefined') {
-			  return acquireVsCodeApi();
-			} else {
-			  const vscode = acquireVsCodeApi();
-			  return vscode;
-			}
-		  }
-		</script>
 		</html>
 	`;
 	
@@ -119,11 +173,22 @@ async function activate(context) {
             vscode.window.showInformationMessage('No text selected');
             return;
         }
+		
+		const fileName = editor.document.fileName;
+		const fileExtension = fileName.split('.').pop();
 
 		const snippetTitle = await vscode.window.showInputBox({
             prompt: 'Enter a title for the snippet',
 			timeout: 10000
         });
+
+		let snippetDescription = await vscode.window.showInputBox({
+			prompt: 'Enter a description for the snippet (optional)'
+		});
+
+		if (snippetDescription === undefined) {
+			snippetDescription = '';
+		}
 
 		if (snippetTitle !== undefined) {
             // Do something with the selected text and snippet title
@@ -143,11 +208,11 @@ async function activate(context) {
         }
 
 		if (snippetTitle !== undefined && selectedText !== undefined) {
-			vscode.window.showInformationMessage(`Title: ${snippetTitle}, Selected Text: ${selectedText}, Tags: ${tags.join(', ')}`);
+			vscode.window.showInformationMessage(`Snippet ${snippetTitle}, Saved Successfully!`);
 
 			const dbFilePath = path.join(__dirname, 'db.json');
 			const data = JSON.parse(fs.readFileSync(dbFilePath, 'utf8'));
-			data.push({ label: snippetTitle, tags: tags, code: selectedText});
+			data.push({ label: snippetTitle, description: snippetDescription, extension: fileExtension ,tags: tags, code: selectedText, cloud: 0});
 			fs.writeFileSync(dbFilePath, JSON.stringify(data, null, 2));
 
 			// Refresh the tree view
@@ -197,36 +262,88 @@ async function activate(context) {
 		  }
 	});
 
-	let postcode_disposable = vscode.commands.registerCommand('Easy-Erreur.pushSnippet_EzzErreur', async (node) => {
+	let postcode_disposable = vscode.commands.registerCommand('Easy-Erreur.pushSnippet', async () => {
 		// Retrieve the stored token from the VSCode Keychain API
 		console.log("Pusing Snippet to Ezz-Erreur ...")
 		const token = context.globalState.get('authToken');
 
-		// let node = {
-		// 	"label": "python",
-		// 	"description": "You can use this for adding a blue button which turns black on hover",
-		// 	"tags": [
-		// 	"python"
-		// 	],
-		// 	"codeSnipet": "def greet():\r\n    name = input(\"Enter your name: \")\r\n    print(\"Hello, {}! Welcome to the Python script.\".format(name))"
-		// }
-	
-		if (token) {
-		  try {
-			const response = await axios.post('https://ezzerreur-hackbyte.onrender.com/codes/postCode',node, {
-			  headers: { 'Authorization': `Bearer ${token}` }
-			});
-			vscode.window.showInformationMessage(response.data.message);
-		  } catch (error) {
-			console.log(error)
-			vscode.window.showErrorMessage('Pushing code failed: ' + error.message);
-		  }
-		} else {
-		  vscode.window.showErrorMessage('Please log in first.');
+		try {
+			if (token) {
+				// Read the content of db.json
+				const dbFilePath = path.join(__dirname, 'db.json');
+				const data = JSON.parse(fs.readFileSync(dbFilePath, 'utf8'));
+		
+				// Extract labels from data
+				const snippetLabels = data.map(item => item.label);
+		
+				// Show a dropdown menu to select a snippet
+				const selectedSnippetLabel = await vscode.window.showQuickPick(snippetLabels, {
+					placeHolder: 'Select a snippet to push'
+				});
+				
+				if (selectedSnippetLabel) {
+					// Find the selected snippet object
+					const selectedSnippet = data.find(item => item.label === selectedSnippetLabel);
+
+					if (selectedSnippet) {
+						console.log("Selected snippet:", selectedSnippet);
+
+						const node = {
+							"title": `${selectedSnippet.label}`,
+							"description": `${selectedSnippet.description}`,
+							"tags": `${selectedSnippet.tags}`,
+							"codeSnipet": `${selectedSnippet.code}`
+						}
+
+						const response = await axios.post('https://ezzerreur-hackbyte.onrender.com/codes/postCode',node, {
+							headers: { 'Authorization': `${token}` }
+						});
+						vscode.window.showInformationMessage(response.data.message);
+					} else {
+						vscode.window.showErrorMessage('Snippet not found!');
+					}
+				}
+			} else {
+				vscode.window.showErrorMessage('Please log in first.');
+			}
+		} catch (error) {
+			vscode.window.showErrorMessage('Error: ' + error.message);
 		}
 	  });
 
-	context.subscriptions.push(disposable, login_disposable, disposable1, webview_disposable, postcode_disposable);
+	  let cleancode_disposable = vscode.commands.registerCommand('Easy-Erreur.cleanCode', async () => {
+		const editor = vscode.window.activeTextEditor;
+        const uncleanCode = editor.document.getText();
+
+		if (!uncleanCode) {
+            vscode.window.showInformationMessage('No text to be cleaned');
+            return;
+        }
+
+		const completion = await openai.chat.completions.create({
+			messages: [
+			  {
+				role: "system",
+				content: "You are a helpful assistant designed to clean my code by removing comments, useless code and make code more readable. also remove statements that we wouldnt like in production - like console.log :",
+			  },
+			  { role: "user", content: `clean this code for me : ${uncleanCode}` },
+			],
+			model: "gpt-3.5-turbo-0125"
+		});
+
+		const cleanedCode = completion.choices[0].message.content;
+
+        // Replace the text in the editor with the cleaned code
+        editor.edit(editBuilder => {
+            const fullRange = new vscode.Range(editor.document.positionAt(0), editor.document.positionAt(uncleanCode.length));
+            editBuilder.replace(fullRange, cleanedCode);
+        });
+
+        vscode.window.showInformationMessage('Code cleaned successfully');
+		
+	  });
+
+	context.subscriptions.push(disposable, login_disposable, disposable1, webview_disposable, postcode_disposable, cleancode_disposable);
 }
 
 // This method is called when your extension is deactivated
--- Extensions/vscode/flatListProvider.js
+++ Extensions/vscode/flatListProvider.js
@@ -3,68 +3,65 @@ const fs = require('fs');
 const path = require('path');
 
 class FlatListProvider {
-  constructor() {
-    this._onDidChangeTreeData = new vscode.EventEmitter();
-    this.onDidChangeTreeData = this._onDidChangeTreeData.event;
-  }
+    constructor() {
+        this._onDidChangeTreeData = new vscode.EventEmitter();
+        this.onDidChangeTreeData = this._onDidChangeTreeData.event;
+    }
 
-  getTreeItem(element) {
-    return {
-      label: element.label,
-      collapsibleState: vscode.TreeItemCollapsibleState.None,
-      command: {
-        command: 'Easy-Erreur.openSnippetView',
-        title: 'Open Snippet',
-        arguments: [element]
-      },
-      buttons: [
-        {
-          tooltip: 'Delete Snippet',
-          iconPath: new vscode.ThemeIcon('trash'),
-          command: {
-            command: 'Easy-Erreur.deleteSnippet',
-            title: 'Delete Snippet',
-            arguments: [element]
-          }
+    getTreeItem(element) {
+        return {
+            label: element.label,
+            collapsibleState: vscode.TreeItemCollapsibleState.None,
+            command: {
+                command: 'Easy-Erreur.openSnippetView',
+                title: 'Open Snippet',
+                arguments: [element]
+            },
+            buttons: [
+                {
+                    tooltip: 'Delete Snippet',
+                    iconPath: new vscode.ThemeIcon('trash'),
+                    command: {
+                        command: 'Easy-Erreur.deleteSnippet',
+                        title: 'Delete Snippet',
+                        arguments: [element]
+                    }
+                }
+            ],
+            iconPath: this.getIconPath(element.extension)
+        };
+    }
+
+    getChildren(element) {
+        const dbFilePath = path.join(__dirname, 'db.json');
+        const data = JSON.parse(fs.readFileSync(dbFilePath, 'utf8'));
+        return Promise.resolve(data.map((item) => new MyTreeNode(item.label, item.description , item.extension, item.tags, item.code)));
+    }
+
+    refresh() {
+        this._onDidChangeTreeData.fire();
+    }
+
+    getIconPath(extension) {
+        switch (extension) {
+            case 'py':
+                return new vscode.ThemeIcon('extensions'); // You can replace 'file-code' with the actual icon you want
+            // Add more cases for other file extensions as needed
+            default:
+                return vscode.ThemeIcon.File;
         }
-      ],
-        iconPath: vscode.ThemeIcon.File
-    };
-  }
-
-  getIconPath(fileExtension) {
-    // Map file extensions to VSCode theme icons
-    const iconMap = {
-      '.js': 'javascript',
-      '.ts': 'typescript',
-      '.py': 'python',
-      '.java': 'java',
-      '.html': 'html',
-      '.css': 'css',
-      '.md': 'markdown',
-    };
-  
-    return iconMap[fileExtension] ? new vscode.ThemeIcon(iconMap[fileExtension]) : new vscode.ThemeIcon('file');
-  }
-
-  getChildren(element) {
-    const dbFilePath = path.join(__dirname, 'db.json');
-    const data = JSON.parse(fs.readFileSync(dbFilePath, 'utf8'));
-    return Promise.resolve(data.map((item) => new MyTreeNode(item.label, item.tags, item.code)));
-  }
-
-  refresh() {
-    this._onDidChangeTreeData.fire();
-  }
+    }
 }
 
 class MyTreeNode {
-  constructor(label, tags, code, onButtonClick) {
-    this.label = label;
-    this.tags = tags;
-    this.code = code;
-    this.onButtonClick = onButtonClick;
-  }
+    constructor(label, description, extension, tags, code, onButtonClick) {
+        this.label = label;
+        this.description = description;
+        this.extension = extension;
+        this.tags = tags;
+        this.code = code;
+        this.onButtonClick = onButtonClick;
+    }
 }
 
 module.exports = FlatListProvider;
