1254b5e6119408edaf529b15f6cc49acf67b454b
Devaa <110590941+anzhong9@users.noreply.github.com>
2024-04-07 06:22:17 +0530

    seo & explore

--- client/src/pages/Explore.jsx
+++ client/src/pages/Explore.jsx
@@ -39,14 +39,24 @@ function Explore() {
             <button className="text-xl rounded-md bg-cyan-950 hover:bg-cyan-500 hover:text-black transition-all duration-100 hover:scale-[1.02] p-1">Add Snippet!</button>
           </div>
         </div>
-        <div className="grid w-full px-3 grid-cols-3 justify-around gap-4">
+        <div className="w-full overflow-y-scroll px-3">
+          <div className="grid grid-cols-3 gap-5" >
+
           {snippets && snippets.length > 0 ? (
+            snippets.map(post => (
+              <SnipCard key={post._id} post={post} />
+              ))
+              ) : (
+                <p>No snippets found</p>
+                )}
+              {snippets && snippets.length > 0 ? (
             snippets.map(post => (
               <SnipCard key={post._id} post={post} />
             ))
           ) : (
             <p>No snippets found</p>
           )}
+          </div>
         </div>
       </div>
     </div>
--- client/src/pages/homepage.jsx
+++ client/src/pages/homepage.jsx
@@ -39,7 +39,9 @@ function Home() {
             <button className="text-xl rounded-md bg-cyan-950 hover:bg-cyan-500 hover:text-black transition-all duration-100 hover:scale-[1.02] p-1">Add Snippet!</button>
           </div>
         </div>
-        <div className="grid w-full px-3 grid-cols-3 overflow-y-scroll h-full justify-around gap-4">
+        <div className="w-full px-3 overflow-y-scroll h-full ">
+          <div className="grid grid-cols-3 gap-5" >
+            
           {snippets && snippets.length > 0 ? (
             snippets.map(post => (
               <SnipCard key={post._id} post={post} />
@@ -47,6 +49,9 @@ function Home() {
           ) : (
             <p>No snippets found</p>
           )}
+          
+          </div>
+          
         </div>
       </div>
     </div>
